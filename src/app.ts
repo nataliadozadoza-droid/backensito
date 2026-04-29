efa83f4e0dd384eac65cd638d688d1e2124ea1ad
nataliadozadoza-droid <nataliadozadoza@gmail.com>
2026-04-29 00:50:01 -0500

    Avance documentacion

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,10 @@ import helmet from 'helmet';
 import v1Routes from "./api/v1/index";
 import { errorMiddleware } from "./middlewares/error.middleware";
 import profileRoutes from "./modules/profile/profile.routes";
-import notificationsRoutes from "./modules/notifications/notification.routers";
-import searchRoutes from "./modules/search/search.routes"
+import notificationsRoutes from "./modules/notifications/notification.routes";
+import searchRoutes from "./modules/search/search.routes";
+import swaggerUi from "swagger-ui-express";
+import { openApiSpec } from './config/openapi';
 
 
 export const app = express();
@@ -14,7 +16,9 @@ export const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(compression());
-app.use(express.json());
+app.use(express.json()); 
+
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(openApiSpec))
 
 app.use("/api/profiles", profileRoutes);
 app.use('/api/v1', v1Routes);
---
+++ src/config/openapi.ts
@@ -0,0 +1,24 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import path from "path"
+
+export const openApiSpec = swaggerJsdoc ({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Profile-administrator-app',
+      description: "Documentacion de la Api",
+      version: '1.0.0',
+    },
+  },
+  servers: [
+
+    {
+      url: "http://localhost:3000/api/v1",
+      description: "Ruta",
+
+    }
+  ],
+  //apis: ['./src/modules/**/*.routes.ts']
+  apis: [path.join(__dirname, './build/modules/**/*.routes.{ts,js}')]
+});
+
